21ac754054358fb1ffba08e1ef18d0e63f62c59e
Nay <138402457+Nayla-Hanifah@users.noreply.github.com>
2024-03-13 23:23:02 +0700

    Update owner-getsesi.js

--- yuhao/owner-getsesi.js
+++ yuhao/owner-getsesi.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File creds.json')
-    let sesi = await fs.readFileSync('./sessions/creds.json')
+    let sesi = await fs.readFileSync('./YuhaoSesi/creds.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m })
 }
 handler.help = ['getsesi']
