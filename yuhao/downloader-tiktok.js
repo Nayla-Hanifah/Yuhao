175a9ab558614718cc8e25b4253430e73dfd1280
Jeff <165579339+Jeffrey-Jeff@users.noreply.github.com>
2024-06-02 16:18:54 +0700

    Update downloader-tiktok.js

--- yuhao/downloader-tiktok.js
+++ yuhao/downloader-tiktok.js
@@ -1,4 +1,3 @@
-let axios = require('axios');
 let { ttdl } = require('btch-downloader')
 
 let handler = async (m, { conn, text }) => {
@@ -23,30 +22,5 @@ handler.tags = ['downloader'];
 handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i;
 handler.limit = false;
 handler.group = false;
-handler.register = true;
 
 module.exports = handler;
-
-const clean = (data) => {
-  let regex = /(<([^>]+)>)/gi;
-  data = data.replace(/(<br?\s?\/>)/gi, " \n");
-  return data.replace(regex, "");
-};
-
-async function shortener(url) {
-  return url;
-}
-
-async function Tiktok(url) {
-  let response = await axios.post("https://lovetik.com/api/ajax/search", new URLSearchParams(Object.entries({ query: url })));
-
-  let result = {};
-  result.creator = "Nayla";
-  result.title = clean(response.data.desc);
-  result.author = clean(response.data.author);
-  result.nowm = await shortener((response.data.links[0].a || "").replace("https", "http"));
-  result.watermark = await shortener((response.data.links[1].a || "").replace("https", "http"));
-  result.audio = await shortener((response.data.links[2].a || "").replace("https", "http"));
-  result.thumbnail = await shortener(response.data.cover);
-  return result;
-    }
